6b16aed4de51cc3be776a0601e5b25309807b8f0
sebasl <sebas@email.com>
2025-09-26 09:34:58 -0500

    update: scaffolding con la tabla Mascota

---
+++ backend/Models/Mascota.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class Mascota
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Especie { get; set; } = null!;
+
+    public string? Raza { get; set; }
+
+    public int Edad { get; set; }
+
+    public string? PhotoUrl { get; set; }
+
+    public int UsuarioId { get; set; }
+
+    public DateTime? CreadoEn { get; set; }
+
+    public DateTime? ActualizadoEn { get; set; }
+
+    public virtual Usuario Usuario { get; set; } = null!;
+}
--- backend/Models/MedipetContext.cs
+++ backend/Models/MedipetContext.cs
@@ -15,6 +15,8 @@ public partial class MedipetContext : DbContext
     {
     }
 
+    public virtual DbSet<Mascota> Mascotas { get; set; }
+
     public virtual DbSet<Rol> Rols { get; set; }
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
@@ -25,6 +27,31 @@ public partial class MedipetContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Mascota>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("mascotas_pkey");
+
+            entity.ToTable("mascotas");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.ActualizadoEn)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("actualizado_en");
+            entity.Property(e => e.CreadoEn)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("creado_en");
+            entity.Property(e => e.Edad).HasColumnName("edad");
+            entity.Property(e => e.Especie).HasColumnName("especie");
+            entity.Property(e => e.Nombre).HasColumnName("nombre");
+            entity.Property(e => e.PhotoUrl).HasColumnName("photo_url");
+            entity.Property(e => e.Raza).HasColumnName("raza");
+            entity.Property(e => e.UsuarioId).HasColumnName("usuario_id");
+
+            entity.HasOne(d => d.Usuario).WithMany(p => p.Mascota)
+                .HasForeignKey(d => d.UsuarioId)
+                .HasConstraintName("fk_usuario");
+        });
+
         modelBuilder.Entity<Rol>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("rol_pkey");
@@ -53,8 +80,6 @@ public partial class MedipetContext : DbContext
             entity.ToTable("usuario");
 
             entity.HasIndex(e => e.Email, "usuario_email_key").IsUnique();
-
-            entity.HasIndex(e => e.Nombre, "usuario_nombre_key").IsUnique();
 
             entity.Property(e => e.Id).HasColumnName("id");
             entity.Property(e => e.ActualizadoEn)
