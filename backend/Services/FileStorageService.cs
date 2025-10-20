25051a2f338dfd2a7200e77415e067bba34dc64e
sebasl <sebas@email.com>
2025-10-20 13:13:37 -0500

    add: creación de api para gestion de imagenes

--- backend/Services/FileStorageService.cs
+++ backend/Services/FileStorageService.cs
@@ -1,3 +1,5 @@
+namespace backend.Services;
+
 public class FileStorageService : IFileStorageService
 {
   private readonly string _uploadsPath;
@@ -16,14 +18,14 @@ public class FileStorageService : IFileStorageService
 
     var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName); // crear un nombre unico para el archivo
     var uploadsPath = Path.Combine(_uploadsPath, fileName); // crear la ruta completa
-                                                                 // Asegurarse de que el directorio existe
+                                                            // Asegurarse de que el directorio existe
 
     using (var newFile = File.Create(uploadsPath)) // crear el archivo
     {
       await file.CopyToAsync(newFile); // copiar el contenido del archivo subido al nuevo archivo
       newFile.Flush(); // asegurar que todo el contenido se ha escrito.
     }
-     var relativePath = $"/uploads/{fileName}"; // ruta relativa para almacenar en la base de datos
+    var relativePath = $"/uploads/{fileName}"; // ruta relativa para almacenar en la base de datos
     return relativePath;
   }
 }
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -44,8 +44,27 @@ export const getPets = async () => {
 };
 
 export const createPet = async (payload) => {
-  // payload: { nombre, especie, raza, edad, photoUrl }
+  /* // payload: { nombre, especie, raza, edad, photoUrl }
   const res = await api.post("/pets", payload);
+  return res.data; */
+  // payload: { nombre, especie, raza, edad, imageUri }
+
+  let imageUrl = null;
+
+  // Si hay imagen, primero la subimos
+  if (payload.imageUri) {
+    imageUrl = await uploadImage(payload.imageUri);
+  }
+
+  // Luego creamos la mascota con la URL
+  const res = await api.post("/pets", {
+    nombre: payload.nombre,
+    especie: payload.especie,
+    raza: payload.raza,
+    edad: payload.edad,
+    imagenUrl: imageUrl, // guardas la URL que devuelve tu backend
+  });
+
   return res.data;
 };
 
@@ -58,3 +77,24 @@ export const updatePet = async (id, payload) => {
   const res = await api.delete(`/pets/${id}`);
   return res.status === 204;
 };*/
+
+export async function uploadImage(imageUri) {
+  const formData = new FormData();
+
+  formData.append("file", {
+    uri: imageUri, // ruta local de la imagen
+    type: "image/jpeg", // o image/png
+    name: "photo.jpg", // nombre del archivo
+  });
+
+  const token = await SecureStore.getItemAsync("token");
+
+  const response = await axios.post(`${API}/filestorage/upload`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data.url; // el backend devuelve { url: "https://..." }
+}
